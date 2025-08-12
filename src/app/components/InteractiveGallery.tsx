5a87d9135bd5028ef37f9b5e7fb1a4cc71be906a
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-08-12 15:25:34 +0530

    Update: Theme Component Updated From ShadCN Docs

---
+++ src/app/components/InteractiveGallery.tsx
@@ -0,0 +1,11 @@
+// components/InteractiveGallery.jsx
+import { useEffect } from 'react'
+import Image from 'next/image'
+
+export default function InteractiveGallery({ 
+  title = "Skills", 
+  images = [], 
+  subtitle = "Services I Do Offer" 
+}) {
+  
+}
