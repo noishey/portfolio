762c180b59e1ff6daddc5a4ada24107a6d422928
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-08-24 15:12:22 +0530

    Debug: Import/Export Errors

--- Desktop/portfolio-template/app/page.tsx
+++ Desktop/portfolio-template/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { ArrowRight } from "lucide-react"
 import Image from "next/image"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "../components/ui/button"
 import DotGridShader from "@/components/DotGridShader"
 import ScrollCard from "@/components/scroll-card"
 import AnimatedHeading from "@/components/animated-heading"
